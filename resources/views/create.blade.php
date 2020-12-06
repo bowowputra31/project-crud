a4fe0e007b2e4e28835e66356429853a4a03bc98
unknown <triwibowoputra49@gmail.com>
2020-12-07 02:45:34 +0900

    project

---
+++ resources/views/create.blade.php
@@ -0,0 +1,41 @@
+@extends('layout.master')
+
+@section('content')
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4>Tambah Data Mahasiswa</h4>
+            </div>
+            <div class="panel-body">
+                <form action="{{url('insert')}}" method="post">
+                    <div class="form-group row">
+                        <label for="id" class="col-sm-2 col-form-label">ID</label>
+                        <div class="col-sm-10">
+                            <input type="text" name="id" id="id" class="form-control" required="require">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="nim" class="col-sm-2 col-form-label">NIM</label>
+                        <div class="col-sm-10">
+                            <input type="text" name="nim" id="nim" class="form-control" required="require">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="nama_lengkap" class="col-sm-2 col-form-label">Nama Lengkap</label>
+                        <div class="col-sm-10">
+                            <input type="text" name="nama_lengkap" id="nama_lengkap" class="form-control">
+                        </div>
+                    </div>                    
+                    <div class="form-group row">
+                        <label for="jurusan" class="col-sm-2 col-form-label">Jurusan</label>
+                        <div class="col-sm-10">
+                             <input type="text" name="jurusan" id="jurusan" class="form-control">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" name="send" id="send" value="Simpan" class="btn btn-success">{!!csrf_field()!!}                       
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
